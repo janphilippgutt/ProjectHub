b0ce003198c5f561f08cc7beb0e7c026a6c87ef9
janphilippgutt <janphilippgutt@posteo.de>
2026-01-04 18:42:42 +0100

    Create projects table

--- migrations/001_create_users.sql
+++ migrations/001_create_users.sql
@@ -4,3 +4,13 @@ CREATE TABLE users (
     role TEXT NOT NULL DEFAULT 'user',
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
+
+CREATE TABLE projects (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    project_description TEXT NOT NULL,
+    image_path TEXT NOT NULL,
+    author_email TEXT NOT NULL,
+    approved BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
